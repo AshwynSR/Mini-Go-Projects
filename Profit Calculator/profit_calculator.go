382c7f4ca79e43c5926b1adebe52be32db76ff7e
Ashwin <ashwinrathore2008@gmail.com>
2025-01-20 20:25:23 +0530

    intro to functions

--- Profit Calculator/profit_calculator.go
+++ Profit Calculator/profit_calculator.go
@@ -2,8 +2,9 @@ package main
 
 import "fmt"
 
+const taxRate float64 = 18
 func main() {
-	const taxRate float64 = 18
+
 	var revenue, expenses float64
 
 	fmt.Print("Enter Revenue Generated: ")
@@ -11,9 +12,20 @@ func main() {
 	fmt.Print("Enter Expenses: ")
 	fmt.Scan(&expenses)
 
-	earningBeforeTax := revenue - expenses
-	profit := earningBeforeTax * (1 - taxRate/100)
-	ratio := earningBeforeTax / profit
+	// earningBeforeTax := revenue - expenses
+	// profit := earningBeforeTax * (1 - taxRate/100)
+	// ratio := earningBeforeTax / profit
+
+	earningBeforeTax, profit, ratio := calculator(revenue, expenses)
+
 
 	fmt.Println("EBT is ", earningBeforeTax, ", Profit is ", profit, " and Ratio was ", ratio)
+	fmt.Printf("EBT is %0.2f, Profit is %0.4f, and Ratio was %0.4f", earningBeforeTax, profit,  ratio)
+}
+
+func calculator(revenue, expenses float64) (ebt float64, profit float64, ratio float64) {
+	ebt = revenue - expenses
+	profit = ebt * (1 - taxRate/100)
+	ratio = ebt / profit
+	return ebt, profit, ratio
 }
