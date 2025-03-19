ade9e09b0ab0b096778307816ae92eb96c51a77f
Ashwin <ashwinrathore2008@gmail.com>
2025-03-19 10:26:01 +0530

    Closures

--- Advanced Functions/main.go
+++ Advanced Functions/main.go
@@ -7,11 +7,19 @@ import (
 func main() {
 	numbers := []int{1, 2, 3}
 
+	double := createTransformer(2)
+	triple := createTransformer(3)
+
 	transformed := transformNumbers(&numbers, func (number int) int  {
 		return number * 2;
 	})
 
+	doubled := transformNumbers(&numbers, double)
+	tripled := transformNumbers(&numbers, triple)
+
 	fmt.Println(transformed)
+	fmt.Println(doubled)
+	fmt.Println(tripled)
 }
 
 func transformNumbers(numbers *[]int, transform func(int) int) []int {
@@ -22,4 +30,10 @@ func transformNumbers(numbers *[]int, transform func(int) int) []int {
 	}
 
 	return dNumbers
+}
+
+func createTransformer(factor int) func (int) int {
+	return func (number int) int  {
+		return number * factor
+	}
 }
