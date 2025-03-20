4463dd19cbeddc184bdd2bcca77387c4077f1e87
Ashwin <ashwinrathore2008@gmail.com>
2025-03-20 20:51:45 +0530

    using multiple channels

--- Concurrency/main.go
+++ Concurrency/main.go
@@ -5,8 +5,9 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChannel chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChannel <- true 
 }
 
 func slowGreet(phrase string, doneChannel chan bool) {
@@ -16,10 +17,17 @@ func slowGreet(phrase string, doneChannel chan bool) {
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	doneChannel := make(chan bool)
-	go slowGreet("How ... are ... you ...?", doneChannel)
-	greet("I hope you're liking the course!")
-	<- doneChannel
+	dones := make([]chan bool, 4)
+	dones[0] = make(chan bool)
+	go greet("Nice to meet you!", dones[0])
+	dones[1] = make(chan bool)
+	go greet("How are you?", dones[1])
+	dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", dones[2])
+	dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", dones[3])
+
+	for _, done := range dones {
+		<- done
+	}
 }
